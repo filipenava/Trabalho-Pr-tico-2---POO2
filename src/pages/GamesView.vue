9429b4055dd659cf7d9f77ee78e40d7dab483a86
Seu NomeFilipe <filipe.nava@unesp.br>
2023-11-23 15:21:36 -0300

    Carrinho de compras atualizado e funcionando simples

--- src/pages/GamesView.vue
+++ src/pages/GamesView.vue
@@ -25,7 +25,10 @@
                   </template>
                 </span>
               </div>
-              <button @click="selectGame(game)">Selecionar</button>
+              <button class="buy-button" :class="{ 'selected': isGameSelected(game) }" @click="selectGame(game)">
+                <span class="icon" v-if="isGameSelected(game)">✔</span>
+                Comprar
+              </button>
             </template>
           </card>
         </div>
@@ -37,8 +40,9 @@
         <div v-if="selectedGames.length > 0">
           <h5>Jogos Selecionados:</h5>
           <ul>
-            <li v-for="game in selectedGames" :key="game.id">{{ game.name }}</li>
+            <li v-for="game in selectedGames" :key="game.id">{{ game.nome }} - R$ {{ game.valor }}</li>
           </ul>
+          <p class="cart-total">Total: R$ {{ cartTotal.toFixed(2) }}</p>
           <button @click="finalizePurchase">Finalizar Compra</button>
           <button @click="clearCart">Limpar Carrinho</button>
         </div>
@@ -68,6 +72,7 @@ export default {
     return {
       selectedGame: "",
       selectedGames: [],
+      cartTotal: 0,
     };
   },
   methods: {
@@ -84,18 +89,27 @@ export default {
       // Implemente o código para lidar com a atualização do jogo aqui
     },
     selectGame(game) {
-      if (this.selectedGames.includes(game)) {
-        this.selectedGames = this.selectedGames.filter(g => g.id !== game.id);
-      } else {
+      const isAlreadySelected = this.selectedGames.some(g => g.id === game.id);
+
+      if (!isAlreadySelected) {
         this.selectedGames.push(game);
+        this.cartTotal += game.valor; // Adicione o valor do jogo ao total do carrinho
+      } else {
+        this.selectedGames = this.selectedGames.filter(g => g.id !== game.id);
+        this.cartTotal -= game.valor; // Subtraia o valor do jogo do total do carrinho
       }
     },
+    isGameSelected(game) {
+      return this.selectedGames.some(g => g.id === game.id);
+    },
     finalizePurchase() {
-      // Implemente a lógica para finalizar a compra aqui
-      alert('Compra finalizada com sucesso!');
+      // Aqui você pode adicionar lógica para processar a compra
+      alert('Compra finalizada com sucesso! Total: R$ ' + this.cartTotal.toFixed(2));
+      this.clearCart(); // Esvaziar o carrinho após a compra
     },
     clearCart() {
       this.selectedGames = [];
+      this.cartTotal = 0; // Redefina o total do carrinho
     },
   },
 
@@ -111,6 +125,38 @@ export default {
   border-radius: 5px;
   margin: 10px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* Estilos para o botão Comprar */
+.game-card .buy-button {
+  padding: 5px 7px;
+  margin-top: 10px;
+  background-color: #28a745; /* Verde */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+}
+
+.game-card .buy-button:hover {
+  background-color: #218838; /* Verde Escuro */
+}
+
+.game-card .buy-button .icon {
+  margin-right: 5px;
+}
+
+/* Estilo quando o jogo está selecionado */
+.game-card .buy-button.selected {
+  background-color: #ffc107; /* Amarelo */
+}
+
+.game-card .buy-button.selected .icon {
+  content: url('/assets/check-icon.png'); /* Caminho para o ícone de check */
 }
 
 /* Estilos para o título do jogo */
@@ -131,49 +177,82 @@ export default {
   position: fixed;
   bottom: 0;
   right: 0;
-  width: 50%; /* Ocupa metade da largura da tela */
-  background-color: #f8f9fa;
-  padding: 10px;
-  border-top: 1px solid #ccc;
-  border-left: 1px solid #ccc;
-  box-shadow: -2px 0 4px rgba(0, 0, 0, 0.1);
-  max-height: 100vh; /* Altura máxima igual à altura da tela */
-  overflow-y: auto; /* Barra de rolagem se necessário */
+  width: 30%; /* Tornar a barra mais estreita */
+  background-color: #fff;
+  padding: 15px;
+  border-top: 3px solid #007bff;
+  box-shadow: -5px 0 10px rgba(0, 0, 0, 0.2);
+  max-height: 100vh;
+  overflow-y: auto;
+  transition: all 0.3s ease-in-out;
 }
 
-.shopping-cart-bar h5,
-.shopping-cart-bar ul,
-.shopping-cart-bar button {
-  text-align: right; /* Alinha os itens à direita */
-}
-
+/* Estilos para cabeçalho e total do carrinho */
 .shopping-cart-bar h5 {
-  margin: 0 0 10px 0;
+  color: #007bff; /* Cor Azul */
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 15px;
 }
 
+.shopping-cart-bar p {
+  color: #333;
+  text-align: left; /* Alinhar à esquerda para melhor legibilidade */
+}
+
+.shopping-cart-bar .cart-total {
+  font-size: 18px;
+  font-weight: bold;
+  color: #28a745; /* Cor Verde */
+  margin-top: 15px;
+  padding-top: 10px;
+  border-top: 1px dashed #ccc;
+}
+
+/* Estilos para lista de jogos no carrinho */
 .shopping-cart-bar ul {
-  list-style: none;
+  list-style-type: none;
   padding: 0;
-  margin: 0 0 10px 0;
+  margin: 0;
 }
 
 .shopping-cart-bar ul li {
-  margin: 5px 0;
+  margin-bottom: 5px;
+  padding-bottom: 5px;
+  border-bottom: 1px solid #eee;
+  font-size: 16px;
 }
 
+/* Estilos para botões */
 .shopping-cart-bar button {
-  padding: 5px 15px;
+  padding: 8px 20px;
   margin-right: 10px;
   background-color: #007bff;
   color: white;
   border: none;
-  border-radius: 3px;
+  border-radius: 5px;
   cursor: pointer;
+  transition: background-color 0.2s;
+  font-size: 16px;
+  margin-top: 10px;
 }
 
 .shopping-cart-bar button:hover {
   background-color: #0056b3;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 }
+
+/* Estilos para o botão Comprar nos cards */
+.game-card .buy-button {
+  /* Estilos existentes */
+  transition: background-color 0.2s;
+}
+
+.game-card .buy-button.selected {
+  /* Estilos existentes */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* Adicionar sombra para destaque */
+}
+
 
 
 
